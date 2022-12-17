6e1a503c408d6f1b69d9ca0d73d301775acd1f30
BigBen3 <benajmin.s.sandler@gmail.com>
2022-12-17 08:28:59 -0800

    Second commit

--- rumors_website_project/rumors/forms.py
+++ rumors_website_project/rumors/forms.py
@@ -12,3 +12,5 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'Community']
+
+
--- rumors_website_project/rumors/views.py
+++ rumors_website_project/rumors/views.py
@@ -3,6 +3,7 @@ from django.views.generic import CreateView, ListView, DetailView
 from .models import Community, Post
 from django.urls import reverse_lazy
 from .forms import CommunityForm, PostForm
+from django.contrib.auth.models import Permission
 
 
 def home(request):
@@ -27,6 +28,12 @@ class CommunitiesDetailView(DetailView):
     template_name = 'rumors/communities.html'
     context_object_name = 'community'
 
+    def join_button(self, request):
+        if request.method == "POST":
+            user = request.user
+            Permission.grant_permission(user , object, 'rumors.post.can_add_post', 'rumors.post.can_remove_post')
+            
+  
 
 class CreateCommunityView(CreateView):
     model = Community
@@ -34,6 +41,7 @@ class CreateCommunityView(CreateView):
     template_name = 'rumors/community_form.html'
     # make sure you know this
     success_url = reverse_lazy("rumors-home")
+
 
 
 class CreatePostView(CreateView):
